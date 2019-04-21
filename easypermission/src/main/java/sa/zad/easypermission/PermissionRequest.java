f170e5af97554c06163b58459f23d28acf6c82aa
Sa.zad Prasla <saijad.prasla@g10consultancy.com>
2019-04-21 23:40:30 +0530

    bg

--- easypermission/src/main/java/sa/zad/easypermission/PermissionRequest.java
+++ easypermission/src/main/java/sa/zad/easypermission/PermissionRequest.java
@@ -9,91 +9,93 @@ import io.reactivex.ObservableSource;
 import io.reactivex.functions.Function;
 
 public class PermissionRequest
-    implements Function<AppPermission, ObservableSource<AppPermissionRequest>> {
+        implements Function<AppPermission, ObservableSource<AppPermissionRequest>> {
 
-  private final Object parent;
-  private Observable<AppPermission> requestResponse;
+    private final Object parent;
+    private Observable<AppPermission> requestResponse;
 
-  public PermissionRequest(Object parent) {
-    this.parent = parent;
-  }
-
-  void setRequestResponse(Observable<AppPermission> requestResponse) {
-    this.requestResponse = requestResponse;
-  }
-
-  public Object getParent() {
-    return parent;
-  }
-
-  public static Activity getActivity(Object parent) {
-    final Activity activity;
-    if (parent instanceof Activity) {
-      activity = ((Activity) parent);
-    } else {
-      activity = ((Fragment) parent).getActivity();
+    public PermissionRequest(Object parent) {
+        this.parent = parent;
     }
-    return activity;
-  }
 
-  @Override
-  public final ObservableSource<AppPermissionRequest> apply(AppPermission appPermission) {
-    if(appPermission.getPermissionStatus(getActivity(parent)) == PermissionUtil.BLOCKED){
-      return Observable.just(new AppPermissionRequest(appPermission, AppPermissionRequest.REQUEST_BLOCKED));
+    public static Activity getActivity(Object parent) {
+        final Activity activity;
+        if (parent instanceof Activity) {
+            activity = ((Activity) parent);
+        } else {
+            activity = ((Fragment) parent).getActivity();
+        }
+        return activity;
     }
-    if(appPermission.isPermissionGranted(getActivity(parent))){
-      return Observable.just(new AppPermissionRequest(appPermission, AppPermissionRequest.ALL_READY_GRANTED));
+
+    void setRequestResponse(Observable<AppPermission> requestResponse) {
+        this.requestResponse = requestResponse;
     }
-    if(!onRequesting(appPermission)){
-      return Observable.just(new AppPermissionRequest(appPermission, AppPermissionRequest.REQUEST_CANCELED));
+
+    public Object getParent() {
+        return parent;
     }
-    return Observable.just(appPermission).flatMap(permission -> {
-          if (showRational(permission)) {
-            return showRationalDialog(permission);
-          }
-          return Observable.just(true);
+
+    @Override
+    public final ObservableSource<AppPermissionRequest> apply(AppPermission appPermission) {
+        if (appPermission.getPermissionStatus(getActivity(parent)) == PermissionUtil.BLOCKED) {
+            return Observable.just(new AppPermissionRequest(appPermission, AppPermissionRequest.REQUEST_BLOCKED));
+        }
+        if (appPermission.isPermissionGranted(getActivity(parent))) {
+            //if in case user has already granted permission from app settings
+            appPermission.onPermissionRequested(getActivity(parent));
+            return Observable.just(new AppPermissionRequest(appPermission, AppPermissionRequest.ALL_READY_GRANTED));
+        }
+        if (!onRequesting(appPermission)) {
+            return Observable.just(new AppPermissionRequest(appPermission, AppPermissionRequest.REQUEST_CANCELED));
+        }
+        return Observable.just(appPermission).flatMap(permission -> {
+            if (showRational(permission)) {
+                return showRationalDialog(permission);
+            }
+            return Observable.just(true);
         })
-        .flatMap(request -> {
-          if (!request){
-            return Observable.just(new AppPermissionRequest(appPermission,AppPermissionRequest.DENIED_RATIONAL));
-          }
-          return requestPerm(parent, appPermission).map(perm -> {
-            if(!perm.isPermissionGranted(getActivity(parent)))
-              return new AppPermissionRequest(appPermission, AppPermissionRequest.DENIED);
-            return new AppPermissionRequest(appPermission, AppPermissionRequest.GRANTED);
-          });
-        });
-  }
-
-  protected boolean onRequesting(AppPermission appPermission) {
-    return !appPermission.isRequestPermissionMaxOut();
-  }
-
-  protected boolean showRational(AppPermission appPermission) {
-    return appPermission.getFailedRequestCount() >= 1;
-  }
-
-  protected Observable<Boolean> showRationalDialog(AppPermission appPermission) {
-    return LibUtils.showDialog(getActivity(parent), appPermission.getPermissions()[0]);
-  }
-
-  protected Observable<AppPermission> requestPerm(final @NonNull Object object,
-                                               AppPermission appPermission) {
-    appPermission.onRequestingPermission(getActivity(object));
-    return requestPerm(object, appPermission.getPermissions());
-  }
-
-  protected Observable<AppPermission> requestPerm(final @NonNull Object object,
-                                               String... permissions) {
-    if (permissions.length > 0) {
-      if (object instanceof Activity) {
-        ActivityCompat.requestPermissions(((Activity) object), permissions,
-            PermissionManager.PERM_REQUEST_CODE);
-      } else {
-        Fragment fragment = ((Fragment) object);
-        fragment.requestPermissions(permissions, PermissionManager.PERM_REQUEST_CODE);
-      }
+                .flatMap(request -> {
+                    if (!request) {
+                        return Observable.just(new AppPermissionRequest(appPermission, AppPermissionRequest.DENIED_RATIONAL));
+                    }
+                    return requestPerm(parent, appPermission).map(perm -> {
+                        if (!perm.isPermissionGranted(getActivity(parent)))
+                            return new AppPermissionRequest(appPermission, AppPermissionRequest.DENIED);
+                        return new AppPermissionRequest(appPermission, AppPermissionRequest.GRANTED);
+                    });
+                });
     }
-    return requestResponse.take(1);
-  }
+
+    protected boolean onRequesting(AppPermission appPermission) {
+        return !appPermission.isRequestPermissionMaxOut();
+    }
+
+    protected boolean showRational(AppPermission appPermission) {
+        return appPermission.getFailedRequestCount() >= 1;
+    }
+
+    protected Observable<Boolean> showRationalDialog(AppPermission appPermission) {
+        return LibUtils.showDialog(getActivity(parent), appPermission.getPermissions()[0]);
+    }
+
+    protected Observable<AppPermission> requestPerm(final @NonNull Object object,
+                                                    AppPermission appPermission) {
+        appPermission.onRequestingPermission(getActivity(object));
+        return requestPerm(object, appPermission.getPermissions());
+    }
+
+    protected Observable<AppPermission> requestPerm(final @NonNull Object object,
+                                                    String... permissions) {
+        if (permissions.length > 0) {
+            if (object instanceof Activity) {
+                ActivityCompat.requestPermissions(((Activity) object), permissions,
+                        PermissionManager.PERM_REQUEST_CODE);
+            } else {
+                Fragment fragment = ((Fragment) object);
+                fragment.requestPermissions(permissions, PermissionManager.PERM_REQUEST_CODE);
+            }
+        }
+        return requestResponse.take(1);
+    }
 }
